d732fce93f7beb36fee843d2ab342f27d40888cb
Chris Jones <gwyn20@gmail.com>
2017-06-08 01:36:44 -0400

    Checkpoint 6 Send Message: Completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,8 @@
 (function() {
-    function HomeCtrl(Room, $uibModal, Message) {
+    function HomeCtrl(Room, $uibModal, Message, $cookies) {
         this.rooms = Room.all;
+        this.currentUser = $cookies.get('blocChatCurrentUser');
+        var currentChatId;
         
         this.modal = function() {
             var modalInstance = $uibModal.open({
@@ -13,13 +15,33 @@
         
         this.currentRoom = function(room) {
             this.currentChatRoom = room.$value;
+            currentChatId = room.$id;
             this.messages = Message.getByRoomId(room.$id);
+        };
+        
+        var m_names =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        var d = new Date();
+        var spl = d.toString().split(' ');
+        var mnth = parseInt(m_names.indexOf(spl[1])+1).toString();
+        mnth = (mnth.length == 1) ? '0'+mnth : mnth
+				//  yyyy       mm      dd       hh:mm:ss 
+        var dateStamp = spl[3]+'/'+mnth+'/'+spl[2]+' -  '+spl[4]
+        
+        this.addMessage = function(message) {
+            var newMessage = {
+                username: $cookies.get('blocChatCurrentUser'),
+                content: this.message,
+                sentAt: dateStamp,
+                roomId: currentChatId
+            };
+            
+            Message.send(newMessage);
+            
         };
     };
     
-    
-
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', 'Message', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', 'Message', '$cookies', HomeCtrl]);
 })();
